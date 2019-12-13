e2802fc433b43e2c1c86657f6ead7c423eb8b0a3
Hardik <hardikgw@gmail.com>
2019-12-13 10:19:14 -0500

    initial commit

---
+++ src/main/java/biz/cits/kafka/KafkaMqSpringApplication.java
@@ -0,0 +1,13 @@
+package biz.cits.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMqSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaMqSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/biz/cits/kafka/KafkaMqSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.cits.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaMqSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
