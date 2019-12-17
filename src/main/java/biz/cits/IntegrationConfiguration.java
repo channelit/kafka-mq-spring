7b87ea30409fcc5c259ec1fb3935b49de54e715d
Hardik <hardikgw@gmail.com>
2019-12-17 03:03:22 -0500

    compiled

--- src/main/java/biz/cits/IntegrationConfiguration.java
+++ src/main/java/biz/cits/IntegrationConfiguration.java
@@ -3,10 +3,12 @@ package biz.cits;
 import org.aspectj.bridge.IMessageHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.expression.common.LiteralExpression;
+import org.springframework.integration.IntegrationMessageHeaderAccessor;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.annotation.Transformer;
 import org.springframework.integration.channel.DirectChannel;
@@ -16,9 +18,18 @@ import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.MessageChannels;
 import org.springframework.integration.handler.LoggingHandler;
 import org.springframework.integration.jms.dsl.Jms;
+import org.springframework.integration.kafka.dsl.Kafka;
+import org.springframework.integration.kafka.dsl.KafkaProducerMessageHandlerSpec;
+import org.springframework.integration.kafka.inbound.KafkaMessageDrivenChannelAdapter;
 import org.springframework.integration.kafka.outbound.KafkaProducerMessageHandler;
 import org.springframework.jms.listener.DefaultMessageListenerContainer;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.listener.AbstractMessageListenerContainer;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.KafkaMessageListenerContainer;
+import org.springframework.kafka.support.DefaultKafkaHeaderMapper;
 import org.springframework.messaging.*;
 
 @Configuration
@@ -28,18 +39,29 @@ public class IntegrationConfiguration {
     @Autowired
     DefaultMessageListenerContainer externalSourceMqListenerContainer;
 
+    @Autowired
+    KafkaMessageListenerContainer<String, String> kafkaListenerContainer;
+
+    @Autowired
+    KafkaTemplate<String, String> kafkaTemplate;
+
+    @Value("${kafka.key}")
+    String kafkaMessageKey;
+
+    @Value("${kafka.topic}")
+    String kafkaTopic;
+
     @Bean
     public IntegrationFlow received() {
         return IntegrationFlows.from(Jms.messageDrivenChannelAdapter(externalSourceMqListenerContainer)
                 .autoStartup(true)
                 .extractPayload(true))
-                .handle("msgConsumer", "consumeMessage")
                 .channel("externalSourceMqChannel")
                 .log(
                         LoggingHandler.Level.INFO, "External Source MQ", m -> m.getHeaders().getId() + ": " + m.getPayload()
                 )
                 .enrichHeaders(eh -> eh.header("MessageType", "demo", true))
-                .channel("toMongoChannel")
+                .channel("toKafka")
                 .get();
     }
 
@@ -47,13 +69,33 @@ public class IntegrationConfiguration {
         return MessageChannels.queue(1).get();
     }
 
-    @ServiceActivator(inputChannel = "toKafkaChannel")
+    @ServiceActivator(inputChannel = "toKafka")
     @Bean
-    public MessageHandler toKafkaChannelHandler(@Qualifier("kafkaTemplate") KafkaTemplate<String, String> kafkaTemplate) {
+    public MessageHandler handler(KafkaTemplate<String, String> kafkaTemplate) {
         KafkaProducerMessageHandler<String, String> handler =
                 new KafkaProducerMessageHandler<>(kafkaTemplate);
-        handler.setMessageKeyExpression(new LiteralExpression("this.properties.getMessageKey()"));
+        handler.setTopicExpression(new LiteralExpression(kafkaTopic));
+//        handler.setMessageKeyExpression(new LiteralExpression(kafkaMessageKey));
         return handler;
+    }
+
+    @Bean
+    public KafkaMessageDrivenChannelAdapter<String, String>
+    adapter(KafkaMessageListenerContainer<String, String> container) {
+        KafkaMessageDrivenChannelAdapter<String, String> kafkaMessageDrivenChannelAdapter =
+                new KafkaMessageDrivenChannelAdapter<>(container, KafkaMessageDrivenChannelAdapter.ListenerMode.record);
+        kafkaMessageDrivenChannelAdapter.setOutputChannel(toMongoChannel());
+        return kafkaMessageDrivenChannelAdapter;
+    }
+
+    @Bean
+    public DefaultKafkaHeaderMapper mapper() {
+        return new DefaultKafkaHeaderMapper();
+    }
+
+    @Bean
+    public PollableChannel consumerChannel() {
+        return new QueueChannel();
     }
 
     @Bean("toMongoChannel")
--- src/main/java/biz/cits/KafkaConfiguration.java
+++ src/main/java/biz/cits/KafkaConfiguration.java
@@ -1,13 +1,21 @@
 package biz.cits;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.KafkaMessageListenerContainer;
+import org.springframework.kafka.support.TopicPartitionOffset;
+
+import java.util.Map;
 
 @Configuration
 @EnableConfigurationProperties(KafkaProperties.class)
@@ -16,15 +24,32 @@ public class KafkaConfiguration {
     @Autowired
     private KafkaProperties kafkaProperties;
 
+    @Value("${kafka.topic}")
+    String kafkaTopic;
+
     @Bean
-    public ProducerFactory<?, ?> producerFactory() {
-        return new DefaultKafkaProducerFactory<>(kafkaProperties.buildProducerProperties());
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> producerProperties = kafkaProperties.buildProducerProperties();
+        producerProperties.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+        return new DefaultKafkaProducerFactory<>(producerProperties);
     }
 
     @Bean
     public KafkaTemplate<String, String> kafkaTemplate() {
-        return new KafkaTemplate(producerFactory());
+        return new KafkaTemplate<>(producerFactory());
     }
 
+    @Bean
+    public KafkaMessageListenerContainer<?, ?> kafkaListenerContainer(@Qualifier("kafkaConsumerFactory") ConsumerFactory<?, ?> kafkaConsumerFactory) {
+        return new KafkaMessageListenerContainer<>(kafkaConsumerFactory,
+                new ContainerProperties(new TopicPartitionOffset(kafkaTopic, 0)));
+    }
+
+    @Bean
+    public ConsumerFactory<?, ?> kafkaConsumerFactory() {
+        Map<String, Object> consumerProperties = kafkaProperties.buildConsumerProperties();
+        consumerProperties.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 15000);
+        return new DefaultKafkaConsumerFactory<>(consumerProperties);
+    }
 
 }
